b06534626fcef3f945eea3a18cc6aa761b66da72
diaxx <handajisalah122@gmail.com>
2025-12-11 11:58:42 +0100

    Add pin mapping for SCDCNC ESP32 Shield

---
+++ firmware/pins_scdc.h
@@ -0,0 +1,22 @@
+// pins_scdc.h
+// Pin mapping for SCDCNC ESP32 Shield
+// Compatible with GRBL_ESP32
+
+#define X_STEP_PIN     5
+#define X_DIR_PIN      6
+#define X_ENABLE_PIN   4
+
+#define Y_STEP_PIN     8
+#define Y_DIR_PIN      9
+#define Y_ENABLE_PIN   7
+
+#define Z_STEP_PIN     18
+#define Z_DIR_PIN      19
+#define Z_ENABLE_PIN   10
+
+#define SPINDLE_ENABLE_PIN 3
+#define SPINDLE_PWM_PIN    3
+
+#define LIMIT_X_PIN    2
+#define LIMIT_Y_PIN    1
+
